f6d4481c9278109b3b3e207f67fe57c884e31c1c
Rômulo Estevam Xavier <romulo.xavier2510@gmail.com>
2025-10-17 00:08:38 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 module.exports = function(eleventyConfig) {
-  // Configuração para a pasta de saída (se você usou 'docs')
-  eleventyConfig.dir: {
-      output: "docs"
-  };
+    
+    // 1. CHAMA a função para copiar a pasta 'css'
+    eleventyConfig.addPassthroughCopy("css"); 
+    
+    // 2. CHAMA a função para copiar a pasta 'img'
+    eleventyConfig.addPassthroughCopy("img"); 
 
-  // 1. Diga ao 11ty para copiar a pasta 'css'
-  eleventyConfig.addPassthroughCopy("css"); 
-  
-  // 2. Diga ao 11ty para copiar a pasta 'img' também (se já não estiver fazendo)
-  eleventyConfig.addPassthroughCopy("img"); 
-
-  return {
-    // ...
-  };
+    return {
+        // 3. RETORNA um objeto de configuração (dir)
+        dir: {
+            // Assumindo que você quer voltar para _site por enquanto
+            output: "_site" 
+        }
+    };
 };
