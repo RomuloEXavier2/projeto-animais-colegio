111e06f4401bb4847bbdbb6e7286a5d4683ed9d8
RomuloEXavier2 <romulo.xavier2510@gmail.com>
2025-10-16 22:53:32 -0300

    Adiciona estilos CSS base e configura Passthrough Copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
-// .eleventy.js
 module.exports = function(eleventyConfig) {
+  // Configuração para a pasta de saída (se você usou 'docs')
+  eleventyConfig.dir: {
+      output: "docs"
+  };
+
+  // 1. Diga ao 11ty para copiar a pasta 'css'
+  eleventyConfig.addPassthroughCopy("css"); 
+  
+  // 2. Diga ao 11ty para copiar a pasta 'img' também (se já não estiver fazendo)
+  eleventyConfig.addPassthroughCopy("img"); 
+
   return {
-    dir: {
-      output: "docs" // <--- Garante que a saída é 'docs'
-    }
+    // ...
   };
 };
